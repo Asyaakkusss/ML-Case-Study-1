104e1c3cce7bba9049160fc0aa2f9dcb20a9f20c
NoahHenriques <noah.henriq@gmail.com>
2024-10-16 14:33:51 -0400

    Added LDA and cleaned up the methods
    Signed-off-by: NoahHenriques <noah.henriq@gmail.com>

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.naive_bayes import GaussianNB
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_val_score
