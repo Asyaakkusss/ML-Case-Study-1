26b3d3d2a6720e688cde990d6ceeca1f19ce5226
NoahHenriques <noah.henriq@gmail.com>
2024-10-23 12:14:54 -0400

    began to add data visualization
    Signed-off-by: NoahHenriques <noah.henriq@gmail.com>

---
+++ study_visuals.py
@@ -0,0 +1,23 @@
+#import classifySpam as classify
+#import evaluateClassifier as eval
+import pandas as pd
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+#visualize email data
+email_data = pd.read_csv('spamTrain1.csv')
+features = email_data.iloc[:, :-1]
+labels = email_data.iloc[:, -1]
+
+data_visualized = TSNE(random_state=1).fit_transform(features)
+
+# Create a scatter plot of the t-SNE transformed data
+plt.figure(figsize=(10, 7))
+cmap = plt.get_cmap('viridis', 2)
+scatter = plt.scatter(data_visualized[:, 0], data_visualized[:, 1], c=labels, cmap=cmap, s=5)
+handles, legend_labels = scatter.legend_elements(prop="colors")
+plt.legend(handles=handles, labels=['Not Spam', 'Spam'], title="Classes")
+plt.title('t-SNE Visualization of Email Data')
+plt.xlabel('t-SNE 1st Dimension')
+plt.ylabel('t-SNE 2nd Dimension')
+plt.show()
